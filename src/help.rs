2c2ff2ffec4f5c4b7c37a02b001d39ef99a67f6e
sushmullur <mullur.sushruth@gmail.com>
2024-02-29 20:47:19 -0800

    Mode two implementation

--- src/help.rs
+++ src/help.rs
@@ -2,9 +2,8 @@ pub fn process_command() {
     println!("Usage: weather <city> <mode>");
     println!("These are the available weather modes: ");
     println!("1: General temperature and weather");
-    println!("2: Rain and snowfall");
-    println!("3: Min, max, and real feel temperatures");
-    println!("4: Hourly forecast");
+    println!("2: Advanced temperature and stats");
+    println!("3: Hourly forecast");
     println!("All units are in the metric system.");
     println!("To convert, enter: convert <quantity> <new unit>");
     println!("Example: convert 23.5 fahrenheit");
--- src/weather.rs
+++ src/weather.rs
@@ -22,6 +22,8 @@ pub async fn process_command(args: core::str::SplitWhitespace<'_>) -> Result<(),
                 let map: HashMap<String, Value> = obj.clone().into_iter().collect();
                 if mode == "1" {
                     mode_one(map);
+                } else if mode == "2" {
+                    mode_two(map);
                 }
             } else {
                 println!("Invalid response.");
@@ -41,7 +43,6 @@ async fn request_api(city_name: String) -> Result<String, Box<dyn std::error::Er
     let id = env::var("ID")?;
     let url = format!("{}q={}&id={}&appid={}", api_url, city_name, id, api_key);
     let body = reqwest::get(&url).await?.text().await?;
-
     Ok(body)
 }
 
@@ -49,14 +50,37 @@ async fn request_api(city_name: String) -> Result<String, Box<dyn std::error::Er
 fn mode_one(map: HashMap<String, Value>) {
     let weather_object = map.get("list").unwrap().as_array().unwrap();
     let closest_forecast = &weather_object[0].as_object().unwrap();
-    let closest_temperature_object = closest_forecast.get("main").unwrap().as_object().unwrap().get("temp");
+    let temp = closest_forecast.get("main").unwrap().as_object().unwrap().get("temp").unwrap().as_f64().unwrap() - 273.15;
     let weather_description = closest_forecast.get("weather").unwrap().as_array().unwrap()[0].as_object().unwrap().get("description").unwrap().as_str().unwrap();
-    let temp_value = match closest_temperature_object {
-        Some(temp_value) => temp_value.as_f64().unwrap_or(0.0),
-        None => 0.0,
-    } - 273.15;
-    let temp_string = format!("The current temperature is {:.2} °C.", temp_value);
+    let temp_string = format!("The current temperature is {:.2} °C.", temp);
     let description_string = format!("The weather is currently: {}.", weather_description);
     println!("{}", temp_string);
     println!("{}", description_string);
+}
+
+// Weather description - Done
+// Temperature, real feel, minimum and max temperature, humidity, and pressure. - Done
+// Clouds, wind speed and direction.
+// Rain percentage (if it is raining)
+fn mode_two(map: HashMap<String, Value>) {
+    let weather_object = map.get("list").unwrap().as_array().unwrap();
+    let closest_forecast = &weather_object[0].as_object().unwrap();
+    let weather_description = closest_forecast.get("weather").unwrap().as_array().unwrap()[0].as_object().unwrap().get("description").unwrap().as_str().unwrap();
+    let temp = closest_forecast.get("main").unwrap().as_object().unwrap().get("temp").unwrap().as_f64().unwrap() - 273.15;
+    let real_feel = closest_forecast.get("main").unwrap().as_object().unwrap().get("feels_like").unwrap().as_f64().unwrap() - 273.15;
+    let temp_min = closest_forecast.get("main").unwrap().as_object().unwrap().get("temp_min").unwrap().as_f64().unwrap() - 273.15;
+    let temp_max = closest_forecast.get("main").unwrap().as_object().unwrap().get("temp_max").unwrap().as_f64().unwrap() - 273.15;
+    let humidity = closest_forecast.get("main").unwrap().as_object().unwrap().get("humidity").unwrap().as_f64().unwrap();
+    let pressure = closest_forecast.get("main").unwrap().as_object().unwrap().get("pressure").unwrap().as_f64().unwrap();
+    let rain_chance = closest_forecast.get("pop").unwrap().as_f64().unwrap() * 100.0;
+    let wind_object = closest_forecast.get("wind").unwrap().as_object().unwrap();
+    let wind_speed = wind_object.get("speed").unwrap().as_f64().unwrap() * 3.6;
+    let wind_direction = wind_object.get("deg").unwrap().as_i64().unwrap();
+    println!("Weather: {}.", weather_description);
+    println!("Temperature: {:.2} °C.\nReal Feel: {:.2} °C.", temp, real_feel);
+    println!("Temperature range: {:.2} °C - {:.2} °C.", temp_min, temp_max);
+    println!("Humidity: {}%.", humidity);
+    println!("Atmospheric pressure: {} hPa.", pressure);
+    println!("Chance of rain: {:.0}%.", rain_chance);
+    println!("Wind speed: {:.2} km/hr.\nWind direction: {}°.", wind_speed, wind_direction);
 }
